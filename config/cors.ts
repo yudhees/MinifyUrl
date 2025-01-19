9ad3b8191cb2a20257ae9290b4ff32fcc8988a1f
k.b.Yudhees Raam Kumar <yudees.raamkumar@bakuun.com>
2025-01-19 19:04:50 +0530

    CORS issue

--- config/cors.ts
+++ config/cors.ts
@@ -7,7 +7,7 @@ import { defineConfig } from '@adonisjs/cors'
  * https://docs.adonisjs.com/guides/security/cors
  */
 const corsConfig = defineConfig({
-  enabled: false,
+  enabled: true,
   origin: '*',
   methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
   headers: true,
